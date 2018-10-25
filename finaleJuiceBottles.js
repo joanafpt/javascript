38826ce54c76ab3dd500d7b1fe6005d05c1d8e6a
Joana <joanajteixeira@outlook.com>
2018-10-25 12:11:53 +0100

    a few changes on the previous code

---
+++ finaleJuiceBottles.js
@@ -0,0 +1,11 @@
+var num = 99;
+while (num > 2) {
+console.log (num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num -1) + " bottles of juice on the wall!");
+num --;
+if (num == 2){
+console.log (num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num -1) + " bottle of juice on the wall!");
+num --;}
+if (num == 1){
+console.log (num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num -1) + " bottles of juice on the wall!");
+num --;}
+}
