eb3892f3711d0b25ec40d0e927f69887eb0fddd2
Joana <joanajteixeira@outlook.com>
2018-10-27 11:41:35 +0100

    Fixing and changing loops

---
+++ changeLoop.js
@@ -0,0 +1,21 @@
+/* Rewrites a while loop as a for loop:
+
+var x = 9;
+while (x >= 1) {
+  console.log("hello " + x);
+  x = x - 1;
+} 
+*/
+
+
+/*
+ * Programming Quiz: Changing the Loop (4-4)
+ */
+
+
+
+var x = 9;
+
+for (var x = 9; x >= 1; x --){
+    console.log("hello " + x);
+}
---
+++ fixTheLoop.js
@@ -0,0 +1,16 @@
+/* Fix the errors in the next for loop:
+for (x < 10; x++) {
+  console.log(x);
+}  */
+
+
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ */
+
+// fix the for loop
+
+
+for (var x = 5; x < 10; x++) {
+    console.log(x);
+}
