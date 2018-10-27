1ba35917a3df036cc6c17a5beaffb8d8ff765986
Joana <joanajteixeira@outlook.com>
2018-10-27 13:35:57 +0100

    a for loop which calculates the factorial of 12

---
+++ factor.js
@@ -0,0 +1,17 @@
+//a for loop that prints the factorial of 12
+
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+
+var solution = 1;
+
+for (var x = 1; x <= 12; x ++){
+
+solution = solution * x;
+
+}
+
+console.log(solution);
