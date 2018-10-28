f9a82cef6d8966986edd57c452b395e1a5d4b233
Joana <joanajteixeira@outlook.com>
2018-10-28 11:08:48 +0000

    function laugh() returns "hahahahahahahahahaha!" and shows the returned value

---
+++ LaughItOff .js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+
+
+
+function laugh(){
+    
+    var message = "hahahahahahahahahaha!";
+    return message;
+}
+
+console.log(laugh());
+
+
