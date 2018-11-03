2f46671e294c9814392c19e185d6db398484e738
Joana <joanajteixeira@outlook.com>
2018-11-03 16:45:57 +0000

    Programming Quiz: Cry (5-5)

---
+++ cry.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ */
+
+// your code goes here
+
+var cry = function cries(boo){
+    
+var message = "";
+
+message = "boohoo!";
+
+return message;
+    
+}
+
+console.log(cry());
