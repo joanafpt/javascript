679a66271fbdc3d1e425b420b76fa719923bfcd2
Joana <joanajteixeira@outlook.com>
2018-11-12 11:01:32 +0000

    Creates an object in JS that represents a breakfast menu

---
+++ objectsMenuItems7_2.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Menu Items (7-2)
+ */
+
+// your code goes here
+
+var breakfast = {
+    
+    name : "The Lumberjack",
+    price: "$9.95",
+    ingredients: ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+    
+};
+
+console.log(breakfast.name);
+console.log(breakfast.price);
+console.log(breakfast.ingredients);
+console.log(breakfast);
